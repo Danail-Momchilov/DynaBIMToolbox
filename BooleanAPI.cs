faa16abab04fb9c33e1753ffbb93a2d96ddca32a
Danail Momchilov <46314846+Danail-Momchilov@users.noreply.github.com>
2023-07-24 18:08:46 +0300

    2023-07-24 - Daily Commit
    - General package structure fixed
    - Transform class introduced
    - Solid extrusion method from rectangle created (bugs need fixing)

--- BooleanAPI.cs
+++ BooleanAPI.cs
@@ -13,6 +13,7 @@ using Revit.Elements;
 using RevitServices.Persistence;
 using System.Xml.Linq;
 using Revit.GeometryConversion;
+using DynaBIMToolbox.Invisible;
 
 
 // TO DO! Research NodeModel nodes with custom UI
@@ -22,30 +23,117 @@ using Revit.GeometryConversion;
 namespace Solids
 {
     // class : interpreted as a subcategory 
-    public class BooleanAPI
+    public class SolidsAPI
     {
         // constructors : labelled as a create node in the subcategory (if private and empty it will not be displayed) : it is preferable to use the "By" keyword, when possible
-        private BooleanAPI() { }
+        private SolidsAPI() { }
 
         // methods : interpreted as a modify node
         /// <summary>
-        /// Extremely fast RevitAPI intersection between solids and surfaces
+        /// Get the Autodesk.DB.Solid for the specified wall
         /// </summary>
-        /// <param name=""> </param>
-        /// <param name=""> </param>
-        /// <returns> List of numbers, representing the area to be deducted from each surface after the intersection </returns>
-        /// <search> surface, solid, intersection, API </search>
-        public static List<Autodesk.Revit.DB.Solid> SurfaceSolidAPIIntersectionAreas(List<Revit.Elements.Wall> hostModelWalls, List<Revit.Elements.Wall> linkedModelWalls)
+        /// <param name="hostModelWall"> Wall instance </param>
+        /// <returns> Autodesk.DB.Solid || The solid, representing the wall Geometry in the API </returns>
+        /// <search> wall, solid, API </search>
+        public static Autodesk.Revit.DB.Solid GetWallSolid(Revit.Elements.Wall hostModelWall)
         {
             try
             {
+                List<Revit.Elements.Wall> wallList = new List<Revit.Elements.Wall> { hostModelWall };
                 // get solids of host model walls
-                List<Autodesk.Revit.DB.Solid> hostedWallsSolids = GetElementSolids.ReturnSolids(hostModelWalls);
+                List<Autodesk.Revit.DB.Solid> hostedWallsSolids = SolidConversions.ReturnSolids(wallList);
 
-                // get solids of linked model walls
-                List<Autodesk.Revit.DB.Solid> linkedWallsSolids = GetElementSolids.ReturnSolids(linkedModelWalls);
+                return hostedWallsSolids[0];
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
 
-                return linkedWallsSolids;
+        /// <summary>
+        /// Get the Autodesk.DB.Solid for the specified wall from linked model
+        /// </summary>
+        /// <param name="linkModelWall"> Wall || A wall instance from linked model </param>
+        /// <param name="linkInstance"> Wall || A wall instance from linked model </param>
+        /// <returns> Autodesk.DB.Solid || The solid, representing the wall Geometry in the API </returns>
+        /// <search> wall, solid, API </search>
+        public static Autodesk.Revit.DB.Solid GetWallSolidTransformed(Revit.Elements.Wall linkModelWall, RevitLinkInstance linkInstance)
+        {
+            try
+            {
+                List<Revit.Elements.Wall> wallList = new List<Revit.Elements.Wall> { linkModelWall };
+                // get solids of host model walls
+                List<Autodesk.Revit.DB.Solid> hostedWallsSolids = SolidConversions.ReturnSolids(wallList);
+
+                return SolidConversions.ReturnTransformedSolid(hostedWallsSolids[0], linkInstance);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Wrap Revit Autodesk.Revit.DB.Solid to get Dynamo solid
+        /// </summary>
+        /// <param name="solid"> Solid || Autodesk.Revit.DB.Solid </param>
+        /// <returns> Autodesk.DesignScript.Geometry || Dynamo Solid </returns>
+        /// <search> solid, API, Dynamo </search>
+        public static Autodesk.DesignScript.Geometry.Solid ReturnDynamoSolid(Autodesk.Revit.DB.Solid solid)
+        {
+            try
+            {
+                return solid.ToProtoType();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Create Autodesk.Revit.DB.Solid, based on a given list of points and height
+        /// </summary>
+        /// <param name="points"> [points] || A list of dynamo points. Note: Points should be exactly four and they should be in the correct order, otherwise the node will return 'null' </param>
+        /// <param name="height"> height || The specified height of the Solid </param>
+        /// <returns> Autodesk.Revit.DB.Solid || Revit API solid </returns>
+        /// <search> solid, API, Dynamo </search>
+        public static Autodesk.Revit.DB.Solid RevitAPIExtrusion(List<Autodesk.DesignScript.Geometry.Point> points, double height)
+        {
+            try
+            {
+                if (points.Count != 4)
+                    return null;
+                else
+                {
+                    return SolidConversions.CreateSolidExtrusion(points, height);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Create Autodesk.Revit.DB.Solid and transform its geometry, based on a given list of points, height and a linkInstance to get its Transform
+        /// </summary>
+        /// <param name="points"> [points] || A list of dynamo points. Note: Points should be exactly four and they should be in the correct order, otherwise the node will return 'null' </param>
+        /// <param name="height"> height || The specified height of the Solid </param>
+        /// <param name="linkInstance"> Link Instance </param>
+        /// <returns> Autodesk.Revit.DB.Solid || Revit API solid </returns>
+        /// <search> solid, API, Dynamo </search>
+        public static Autodesk.Revit.DB.Solid RevitAPIExtrusionTransform(List<Autodesk.DesignScript.Geometry.Point> points, double height, RevitLinkInstance linkInstance)
+        {
+            try
+            {
+                if (points.Count != 4)
+                    return null;
+                else
+                {
+                    return SolidConversions.ReturnTransformedSolid(SolidConversions.CreateSolidExtrusion(points, height), linkInstance);
+                }
             }
             catch (Exception e)
             {
---
+++ Invisible/SolidConversions.cs
@@ -0,0 +1,115 @@
+﻿using Autodesk.DesignScript.Geometry;
+using System.Collections.Generic;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+
+namespace DynaBIMToolbox.Invisible
+{
+    // with the IsVisibleInDynamoLibrary function, the class remains hidden and can only be used internally
+    [IsVisibleInDynamoLibrary(false)]
+    public class SolidConversions
+    {
+        public SolidConversions() { }
+
+        public static List<Autodesk.Revit.DB.Solid> ReturnSolids(List<Revit.Elements.Wall> hostModelWalls)
+        {
+            try
+            {
+                Options options = new Options();
+                List<Autodesk.Revit.DB.Solid> hostWallSolids = new List<Autodesk.Revit.DB.Solid>();
+
+                foreach (Revit.Elements.Wall wall in hostModelWalls)
+                {
+                    Element revitWall = wall.InternalElement;
+
+                    foreach (GeometryObject geoEle in revitWall.get_Geometry(options))
+                    {
+                        if (geoEle is GeometryInstance)
+                        {
+                            GeometryInstance geoInst = geoEle as GeometryInstance;
+                            GeometryElement geoSet = geoInst.GetInstanceGeometry();
+
+                            foreach (GeometryObject geoObj in geoSet)
+                            {
+                                if (geoObj is Autodesk.Revit.DB.Solid)
+                                {
+                                    Autodesk.Revit.DB.Solid geoSolid = geoObj as Autodesk.Revit.DB.Solid;
+
+                                    if (geoSolid.Volume > 0)
+                                    {
+                                        hostWallSolids.Add(geoSolid);
+                                    }
+                                }
+                            }
+                        }
+                        else if (geoEle is Autodesk.Revit.DB.Solid)
+                        {
+                            Autodesk.Revit.DB.Solid geoSolid = geoEle as Autodesk.Revit.DB.Solid;
+
+                            if (geoSolid.Volume > 0)
+                            {
+                                hostWallSolids.Add(geoSolid);
+                            }
+                        }
+                    }
+                }
+
+                return hostWallSolids;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        
+        public static Autodesk.Revit.DB.Solid ReturnTransformedSolid(Autodesk.Revit.DB.Solid solid, RevitLinkInstance linkInstance)
+        {
+            try
+            {
+                Document linkDocument = linkInstance.GetLinkDocument();
+
+                // create transform to get the link instance 
+                Transform coordinateSystemTransform = Transform.Identity;
+
+                // get the coordinate system information from the link instance
+                XYZ origin = linkInstance.GetTotalTransform().Origin;
+                XYZ xAxis = linkInstance.GetTotalTransform().BasisX;
+                XYZ yAxis = linkInstance.GetTotalTransform().BasisY;
+                XYZ zAxis = linkInstance.GetTotalTransform().BasisZ;
+
+                // set the translation and rotation in the transform
+                coordinateSystemTransform.Origin = origin;
+                coordinateSystemTransform.BasisX = xAxis;
+                coordinateSystemTransform.BasisY = yAxis;
+                coordinateSystemTransform.BasisZ = zAxis;
+
+                Autodesk.Revit.DB.Solid transformedSolid = SolidUtils.CreateTransformed(solid, coordinateSystemTransform);
+                return transformedSolid;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static Autodesk.Revit.DB.Solid CreateSolidExtrusion(List<Autodesk.DesignScript.Geometry.Point> points, double height)
+        {
+            XYZ point0 = new XYZ(points[0].X / 30.48, points[0].Y / 30.48, points[0].Z / 30.48);
+            XYZ point1 = new XYZ(points[1].X / 30.48, points[1].Y / 30.48, points[1].Z / 30.48);
+            XYZ point2 = new XYZ(points[2].X / 30.48, points[2].Y / 30.48, points[2].Z / 30.48);
+            XYZ point3 = new XYZ(points[3].X / 30.48, points[3].Y / 30.48, points[3].Z / 30.48);
+
+            Autodesk.Revit.DB.Curve edge0 = Autodesk.Revit.DB.Line.CreateUnbound(point0, point1);
+            Autodesk.Revit.DB.Curve edge1 = Autodesk.Revit.DB.Line.CreateUnbound(point1, point2);
+            Autodesk.Revit.DB.Curve edge2 = Autodesk.Revit.DB.Line.CreateUnbound(point2, point3);
+            Autodesk.Revit.DB.Curve edge3 = Autodesk.Revit.DB.Line.CreateUnbound(point3, point0);
+
+            List<Autodesk.Revit.DB.Curve> edges = new List<Autodesk.Revit.DB.Curve> { edge0, edge1, edge2, edge3 };
+            CurveLoop crvLoop = CurveLoop.Create(edges);
+            List<CurveLoop> crvLoopList = new List<CurveLoop> { crvLoop };
+
+            return GeometryCreationUtilities.CreateExtrusionGeometry(crvLoopList, XYZ.BasisZ, height);
+        }
+    }
+}
